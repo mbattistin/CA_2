0a1efe0f80e8075363926936dbaac1c25683d34e
Mariana Casarin <mariana.battistin@gmail.com>
2025-04-23 12:43:25 +0100

    Implementation of department class

---
+++ hospitalApplication/src/hospitalapplication/classes/Department.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitalapplication.classes;
+
+/**
+ *
+ * @author Mariana
+ */
+public class Department {
+    private String departmentName;
+
+    public Department(String departmentName) {
+        this.departmentName = departmentName;
+    }
+    
+}
