cf142277134e82272be02c4ee60855a5edd16577
Mariana Casarin <mariana.battistin@gmail.com>
2025-04-23 12:27:47 +0100

    Implementation of the class Peson.

---
+++ hospitalApplication/src/hospitalapplication/classes/Person.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitalapplication.classes;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Mariana
+ */
+public class Person {
+    private String name;
+    private Date DateOfBirth;
+}
