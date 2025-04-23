4d10a3e0c65379d12b713ac79b36d19c45ba0a3d
Mariana Casarin <mariana.battistin@gmail.com>
2025-04-23 12:57:42 +0100

    Implementation of the manager class.

---
+++ hospitalApplication/src/hospitalapplication/classes/Manager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitalapplication.classes;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Mariana
+ */
+public class Manager extends Employee{
+    
+    public Manager(Role role, String name, Date dateOfBirth, Date startDate) {
+        super(role, name, dateOfBirth, startDate);
+    }
+    
+}
